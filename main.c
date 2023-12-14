7cd931d8698f25b54cc68751399b5c0dc4fd5b35
vinicius-yoshioka <vinicius.yoshioka@sga.pucminas.br>
2023-12-14 14:44:58 -0300

    Reodenado funções

--- main.c
+++ main.c
@@ -30,49 +30,52 @@ Mensagem buffer_client_at_process;
 sem_t semaforo_thread_main;
 sem_t semaforo_thread_client;
 
-void server(int readfd, int writefd)
+void calcular_resultado(Mensagem *buffer_client_at_thread, Mensagem *buffer_client_at_process)
 {
-	pthread_t client_thread;
-	pthread_create(&client_thread, NULL, client_at_thread, NULL);
+	strcpy(buffer_client_at_thread->tipo, TIPO_RESULTADO);
+	strcpy(buffer_client_at_process->tipo, TIPO_RESULTADO);
 
-	sem_init(&semaforo_thread_main, 0, 0);
-	sem_init(&semaforo_thread_client, 0, 0);
-
-	while (1)
+	if (strcmp(buffer_client_at_thread->valor, buffer_client_at_process->valor) == 0)
 	{
-		strcpy(buffer_client_at_process.tipo, TIPO_JOGAR);
-		strcpy(buffer_client_at_process.valor, "\0");
-		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
-		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
-
-		strcpy(buffer_client_at_thread.tipo, TIPO_JOGAR);
-		strcpy(buffer_client_at_thread.valor, "\0");
-		printf("Server > Client (thread): %s\n", buffer_client_at_thread.tipo);
-
-		sem_post(&semaforo_thread_client);
-		sem_wait(&semaforo_thread_main);
-		printf("Client (thread) > Server: %s\n", buffer_client_at_thread.valor);
-
-		read(readfd, &buffer_client_at_process, sizeof(Mensagem));
-		printf("Client (subprocesso) > Server: %s\n", buffer_client_at_process.valor);
-
-		calcular_resultado(&buffer_client_at_thread, &buffer_client_at_process);
-
-		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.valor);
-		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
-
-		printf("Server > Client (thread): %s\n", buffer_client_at_thread.valor);
-		sem_post(&semaforo_thread_client);
-		sem_wait(&semaforo_thread_main);
-
-		strcpy(buffer_client_at_process.tipo, TIPO_SAIR);
-		strcpy(buffer_client_at_process.valor, "\0");
-		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
-		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
-
-		sem_destroy(&semaforo_thread_main);
-		sem_destroy(&semaforo_thread_client);
-		break;
+		strcpy(buffer_client_at_thread->valor, RESULTADO_EMPATE);
+		strcpy(buffer_client_at_process->valor, RESULTADO_EMPATE);
+		printf("Server: Empate!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PEDRA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_TESOURA) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
+		printf("Server: Pedra quebra tesoura. Client (thread) venceu!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_TESOURA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PEDRA) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
+		printf("Server: Pedra quebra tesoura. Client (subprocesso) venceu!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PAPEL) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PEDRA) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
+		printf("Server: Papel embrulha pedra. Client (thread) venceu!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PEDRA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PAPEL) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
+		printf("Server: Papel embrulha pedra. Client (subprocesso) venceu!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_TESOURA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PAPEL) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
+		printf("Server: Tesoura corta papel. Client (thread) venceu!\n");
+	}
+	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PAPEL) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_TESOURA) == 0)
+	{
+		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
+		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
+		printf("Server: Tesoura corta papel. Client (subprocesso) venceu!\n");
 	}
 }
 
@@ -161,52 +164,49 @@ void client_at_process(int readfd, int writefd)
 	}
 }
 
-void calcular_resultado(Mensagem *buffer_client_at_thread, Mensagem *buffer_client_at_process)
+void server(int readfd, int writefd)
 {
-	strcpy(buffer_client_at_thread->tipo, TIPO_RESULTADO);
-	strcpy(buffer_client_at_process->tipo, TIPO_RESULTADO);
+	pthread_t client_thread;
+	pthread_create(&client_thread, NULL, client_at_thread, NULL);
 
-	if (strcmp(buffer_client_at_thread->valor, buffer_client_at_process->valor) == 0)
+	sem_init(&semaforo_thread_main, 0, 0);
+	sem_init(&semaforo_thread_client, 0, 0);
+
+	while (1)
 	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_EMPATE);
-		strcpy(buffer_client_at_process->valor, RESULTADO_EMPATE);
-		printf("Server: Empate!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PEDRA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_TESOURA) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
-		printf("Server: Pedra quebra tesoura. Client (thread) venceu!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_TESOURA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PEDRA) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
-		printf("Server: Pedra quebra tesoura. Client (subprocesso) venceu!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PAPEL) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PEDRA) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
-		printf("Server: Papel embrulha pedra. Client (thread) venceu!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PEDRA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PAPEL) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
-		printf("Server: Papel embrulha pedra. Client (subprocesso) venceu!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_TESOURA) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_PAPEL) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_VITORIA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_DERROTA);
-		printf("Server: Tesoura corta papel. Client (thread) venceu!\n");
-	}
-	else if (strcmp(buffer_client_at_thread->valor, JOGADA_PAPEL) == 0 && strcmp(buffer_client_at_process->valor, JOGADA_TESOURA) == 0)
-	{
-		strcpy(buffer_client_at_thread->valor, RESULTADO_DERROTA);
-		strcpy(buffer_client_at_process->valor, RESULTADO_VITORIA);
-		printf("Server: Tesoura corta papel. Client (subprocesso) venceu!\n");
+		strcpy(buffer_client_at_process.tipo, TIPO_JOGAR);
+		strcpy(buffer_client_at_process.valor, "\0");
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		strcpy(buffer_client_at_thread.tipo, TIPO_JOGAR);
+		strcpy(buffer_client_at_thread.valor, "\0");
+		printf("Server > Client (thread): %s\n", buffer_client_at_thread.tipo);
+
+		sem_post(&semaforo_thread_client);
+		sem_wait(&semaforo_thread_main);
+		printf("Client (thread) > Server: %s\n", buffer_client_at_thread.valor);
+
+		read(readfd, &buffer_client_at_process, sizeof(Mensagem));
+		printf("Client (subprocesso) > Server: %s\n", buffer_client_at_process.valor);
+
+		calcular_resultado(&buffer_client_at_thread, &buffer_client_at_process);
+
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.valor);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		printf("Server > Client (thread): %s\n", buffer_client_at_thread.valor);
+		sem_post(&semaforo_thread_client);
+		sem_wait(&semaforo_thread_main);
+
+		strcpy(buffer_client_at_process.tipo, TIPO_SAIR);
+		strcpy(buffer_client_at_process.valor, "\0");
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		sem_destroy(&semaforo_thread_main);
+		sem_destroy(&semaforo_thread_client);
+		break;
 	}
 }
 
