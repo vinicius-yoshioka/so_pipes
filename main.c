002d8fbb4d907934c16bcbb70b74988f5a7c8383
vinicius-yoshioka <vinicius.yoshioka@sga.pucminas.br>
2023-12-14 14:41:28 -0300

    Atualizado programa para ter 1 thread e 1 processo

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #define BUFFER_SIZE 1024
 #define TIPO_JOGAR "jogar"
@@ -21,6 +23,12 @@ typedef struct _Mensagem
 	char tipo[BUFFER_SIZE];
 	char valor[BUFFER_SIZE];
 } Mensagem;
+
+Mensagem buffer_client_at_thread;
+Mensagem buffer_client_at_process;
+
+sem_t semaforo_thread_main;
+sem_t semaforo_thread_client;
 
 void server(int readfd, int writefd);
 void *client_at_thread(void *arg);
@@ -72,13 +80,92 @@ int main()
 	return 0;
 }
 
-/*
- * Executa nos processos filhos
- *
- * @param readfd: leitura do pipe pai para filho
- * @param writefd: escrita no pipe filho para pai
- **/
-void client(char *name, int readfd, int writefd)
+void server(int readfd, int writefd)
+{
+	pthread_t client_thread;
+	pthread_create(&client_thread, NULL, client_at_thread, NULL);
+
+	sem_init(&semaforo_thread_main, 0, 0);
+	sem_init(&semaforo_thread_client, 0, 0);
+
+	while (1)
+	{
+		strcpy(buffer_client_at_process.tipo, TIPO_JOGAR);
+		strcpy(buffer_client_at_process.valor, "\0");
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		strcpy(buffer_client_at_thread.tipo, TIPO_JOGAR);
+		strcpy(buffer_client_at_thread.valor, "\0");
+		printf("Server > Client (thread): %s\n", buffer_client_at_thread.tipo);
+
+		sem_post(&semaforo_thread_client);
+		sem_wait(&semaforo_thread_main);
+		printf("Client (thread) > Server: %s\n", buffer_client_at_thread.valor);
+
+		read(readfd, &buffer_client_at_process, sizeof(Mensagem));
+		printf("Client (subprocesso) > Server: %s\n", buffer_client_at_process.valor);
+
+		calcular_resultado(&buffer_client_at_thread, &buffer_client_at_process);
+
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.valor);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		printf("Server > Client (thread): %s\n", buffer_client_at_thread.valor);
+		sem_post(&semaforo_thread_client);
+		sem_wait(&semaforo_thread_main);
+
+		strcpy(buffer_client_at_process.tipo, TIPO_SAIR);
+		strcpy(buffer_client_at_process.valor, "\0");
+		printf("Server > Client (subprocesso): %s\n", buffer_client_at_process.tipo);
+		write(writefd, &buffer_client_at_process, sizeof(buffer_client_at_process));
+
+		sem_destroy(&semaforo_thread_main);
+		sem_destroy(&semaforo_thread_client);
+		break;
+	}
+}
+
+void *client_at_thread(void *arg)
+{
+	while (1)
+	{
+		sem_wait(&semaforo_thread_client);
+
+		if (strcmp(buffer_client_at_thread.tipo, TIPO_JOGAR) == 0)
+		{
+			strcpy(buffer_client_at_thread.tipo, TIPO_JOGADA);
+
+			srand(time(NULL) ^ (getpid() << 16));
+			int jogada = rand() % 3;
+			switch (jogada)
+			{
+			case 0:
+				strcpy(buffer_client_at_thread.valor, JOGADA_PEDRA);
+				break;
+			case 1:
+				strcpy(buffer_client_at_thread.valor, JOGADA_PAPEL);
+				break;
+			case 2:
+				strcpy(buffer_client_at_thread.valor, JOGADA_TESOURA);
+				break;
+			}
+		}
+		else if (strcmp(buffer_client_at_thread.tipo, TIPO_RESULTADO) == 0)
+		{
+			if (strcmp(buffer_client_at_thread.valor, RESULTADO_VITORIA) == 0)
+				printf("Client (thread): %s\n", RESULTADO_VITORIA);
+			else if (strcmp(buffer_client_at_thread.valor, RESULTADO_DERROTA) == 0)
+				printf("Client (thread): %s\n", RESULTADO_DERROTA);
+			else
+				printf("Client (thread): %s\n", RESULTADO_EMPATE);
+		}
+
+		sem_post(&semaforo_thread_main);
+	}
+}
+
+void client_at_process(int readfd, int writefd)
 {
 	Mensagem buffer;
 
